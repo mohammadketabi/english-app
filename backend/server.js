7945aa0296b15af81fc3ffa70388db6626f7620a
mohammadketabi <mohammad.ketabi@gmail.com>
2022-10-18 13:38:22 -0400

    add user schema

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${PORT}`));
